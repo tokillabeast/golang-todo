f4f553409488b572c21dabb45eb00a65179a5b4d
tokillamockingbird <tokillamockingbird1989@gmail.com>
2017-03-13 21:32:03 +0300

    chi router
    use chi router for hello-world example

--- test.go
+++ test.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"io"
 	"net/http"
+
+	"github.com/pressly/chi"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
-}
-
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe("0.0.0.0:8000", nil)
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe("0.0.0.0:8000", r)
 }
